ba36075f0a09871dce2411ad4af9af4bff00aec3
Peggy Griffin <peggysuegriffin@gmail.com>
2014-03-08 20:32:13 -0700

    Passed 5th test

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -28,4 +28,5 @@ end
      puts "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
   else
      puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
+     puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
 end
