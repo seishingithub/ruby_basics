734ce4b9dae0e15668edf830db1f2d0c46e9a450
Peggy Griffin <peggysuegriffin@gmail.com>
2014-03-08 20:29:36 -0700

    Passed tests 2-4

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -19,8 +19,13 @@ verse_number = gets.chomp
 verse_number = verse_number.to_i
 
 # Sing the song and subtract one bottle
-while verse_number > 1
+while verse_number > 2
   puts "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n"
 verse_number -= 1
-puts "Take one down and pass it around, #{verse_number} bottles of beer on the wall.\n"
+  puts "Take one down and pass it around, #{verse_number} bottles of beer on the wall.\n"
+end
+  if verse_number == 2
+     puts "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+  else
+     puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
 end
