48e0ae2dea120d812216437889094f842cfbe914
Peggy Griffin <peggysuegriffin@gmail.com>
2014-03-09 01:40:11 -0700

    Fixed bug involving 'if verse_number == 2' that was incorrectly passing rspec

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -30,8 +30,10 @@ verse_number -= 1
 end
   if verse_number == 2
      puts "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+     puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
+     puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
   else
      puts "1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no bottles of beer on the wall.\n"
      puts "No bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
-  end
+end
