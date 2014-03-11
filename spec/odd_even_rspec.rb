f0a7d943d36e5b94552e9073a7bb227708a79e12
Peggy Griffin <seishingithub@gmail.com>
2014-03-11 17:18:32 -0600

    created test to replace odd numbers in the array with the text 'Odd'

--- spec/odd_even_rspec.rb
+++ spec/odd_even_rspec.rb
@@ -1,34 +1,5 @@
-=begin
-> The class will return the numbers starting with 1 and ending with
-the number that it was initialized with when the run method is called.
-For example, if I pass in 10, then the program will return an array
-containing 1 through 10.
-> If the number is even then the program will replace the number in
-the array with the text "Even".
-> If the number is odd, then the program will replace the number in
-the array with the text "Odd".
-=end
-
-=begin
-require "rspec/core"
-require_relative "../bin/key_value_store"
-
-describe KeyValue do
-  it "allows user of the class to add a value stored under a key" do
-    key_value = KeyValueStore.new
-    expected = {:key => value}
-    actual = key_value.add
-    expect(actual).to eq expected
-  end
-end
-=end
-
-
-
 require "rspec/core"
 require "./bin/odd_even"
-
-
 
 describe NumSequence do        # This is the constant
   it "return #s 1 up to # initialized when run method is called" do
@@ -43,6 +14,14 @@ describe NumSequence do
   it "replaces even numbers in the array with the text 'Even'" do
     high_number = NumSequence.new(8)
     expected = [1, "Even", 3, "Even", 5, "Even", 7, "Even"]
+    actual = high_number.arrayify
+  end
+end
+
+describe NumSequence do
+  it "replaces odd numbers in the array with the text 'Odd'" do
+    high_number = NumSequence.new(10)
+    expected = ["Odd", "Even", "Odd", "Even", "Odd", "Even", "Odd", "Even", "Odd", "Even"]
     actual = high_number.arrayify
   end
 end
